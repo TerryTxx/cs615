63568376bd05c24b665bd5179711be8625b068ce
FelicitaFlorence <fel577e@gmail.com>
2024-04-14 22:47:37 +0100

    Update dragdrop.js

--- client/src/components/dragdrop.js
+++ client/src/components/dragdrop.js
@@ -1,10 +1,11 @@
 import $ from 'jquery'
 import 'jquery-ui-dist/jquery-ui';
 $(".mcell-task").draggable({
-  appendTo: "body",
-  cursor: "move",
-  helper: 'clone',
-  revert: "invalid"
+  appendTo: "body", // Append the dragged element to the body
+  cursor: "move", // Set cursor style to move while dragging
+  helper: 'clone', // Clone the dragged element
+  revert: "invalid" // Revert the dragged element if not dropped on a valid droppable target
+
 });
 $(".mcell").droppable({
   tolerance: "intersect",
