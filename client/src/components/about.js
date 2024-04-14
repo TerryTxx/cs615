03d0281cdcd06498a6fad06143fcff6e99baaf4f
FelicitaFlorence <fel577e@gmail.com>
2024-04-14 22:40:35 +0100

    Update about.js

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -11,11 +11,11 @@ class About extends Component{
                   <aside className="container">
                     <div className="col-sm aboutUs">
                         <h2 className="mcell-title story">Abous Us</h2>
-                        <div className="padding20">@Xiaoxu Tan</div>
-                        <div className="padding20">@Xinming Liao</div>
-                        <div className="padding20">@Sarbojit Bhattacharjee</div>
-                        <div className="padding20">@Felicita Florence Clement</div>
-                        <div className="padding20">@Steffi Evangeline Banans</div>
+                        <div className="padding21">@Xiaoxu Tan</div>
+                        <div className="padding21">@Xinming Liao</div>
+                        <div className="padding21">@Sarbojit Bhattacharjee</div>
+                        <div className="padding21">@Felicita Florence Clement</div>
+                        <div className="padding21">@Steffi Evangeline Banans</div>
                     </div>
                   </aside>
             </div>
