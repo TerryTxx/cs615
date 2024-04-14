473133c65e2a4c817f4178529511fc95fa9e177f
FelicitaFlorence <fel577e@gmail.com>
2024-04-15 00:11:25 +0100

    Update about.js

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -18,8 +18,17 @@ class About extends Component{
                         <div className="padding21">@Steffi Evangeline Banans</div>
                     </div>
                   </aside>
+                  <div class="col-sm aboutUs">
+        <h2 class="mcell-title story">Our Mission</h2>
+        <div class="padding21">Empower teams to achieve their full potential by providing a seamless and intuitive platform for collaboration</div>
+        <div class="padding21">we aim to redefine the way teams collaborate and unlock new levels of efficiency and success in the modern workplace.</div>
+        <div class="padding21"><h2>Unleash Team Synergy, One Task at a Time.</h2></div>
+    </div>
             </div>
+
+
         )
+
     }
 }
 export default About
