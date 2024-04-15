8df48ca4350c7545e50da909c9ff59411a26ff06
23252356-sarbojit bhattacharjee <SARBOJIT.BHATTACHARJEE.2024@mumail.ie>
2024-04-15 15:01:52 +0100

    Update scrumuser.js

--- server/routes/scrumuser.js
+++ server/routes/scrumuser.js
@@ -5,14 +5,17 @@ const User = require('../models/ScrumUser');
 const router = express.Router();
 const SECRET_KEY = "your_secret_key";
 
+// Register a new user
 router.post('/register', async (req, res) => {
     try {
         const { fullName, username, password, email } = req.body;
+        // Hash the password before saving
+        const hashedPassword = await bcrypt.hash(password, 8);
 
         const newUser = new User({
             fullName,
             username,
-            password,
+            password: hashedPassword,
             email
         });
 
@@ -23,16 +26,19 @@ router.post('/register', async (req, res) => {
     }
 });
 
+// User login
 router.post('/login', async (req, res) => {
     try {
         const { username, password } = req.body;
         const user = await User.findOne({ username });
 
         if (!user) {
-            return res.status(404).send('no user');
+            return res.status(404).send('User not found');
         }
-        if (password !== user.password) {
-            return res.status(400).send('password no');
+        // Compare hashed password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).send('Invalid password');
         }
         const token = jwt.sign(
             { id: user._id },
@@ -44,6 +50,5 @@ router.post('/login', async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 });
-
 
 module.exports = router;
